20e1c22ce87e3c314139de1f20999ae7515526d6
DimiVaksman <vde92@hotmail.com>
2023-04-11 20:37:20 +0200

    lesson 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,10 @@
+import { RecipeList } from "./RecipeList";
+import recipes from "../recipes.json";
+
 export const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <RecipeList items={recipes} />
+    </>
   );
 };
